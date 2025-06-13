a64038658f66693808f6f3dde1e8747047118d53
alexjiao <alex.jiao@keysight.com>
2025-06-13 15:56:05 +0800

    Modify nr_uci_encoding for UCI on PUSCH
    38.212 section 5.3.3.1 and 5.3.3.2 describe how to encode 1-bit and 2-bit information.
    This is needed when typically HARQ-ACK will be transmitted on PUSCH.
    38.212 section 5.4.3 also describes how to do rate matching for channel coding of small block lengths.

--- openair1/PHY/CODING/nrSmallBlock/encodeSmallBlock.c
+++ openair1/PHY/CODING/nrSmallBlock/encodeSmallBlock.c
@@ -31,11 +31,14 @@
 */
 
 #include "PHY/CODING/nrSmallBlock/nr_small_block_defs.h"
+#include "common/utils/assertions.h"
 
 //input = [0 ... 0 c_K-1 ... c_2 c_1 c_0]
 //output = [d_31 d_30 ... d_2 d_1 d_0]
 uint32_t encodeSmallBlock(int in, int len)
 {
+  AssertFatal(len >= 3, "encodeSmallBlock only supports input lengths A >= 3, got A=%d", len);
+
   uint32_t out = 0;
   for (int i = 0; i < len; i++)
     if ((in & (1 << i)) > 0)
