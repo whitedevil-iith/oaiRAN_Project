turkeryilmaz/openairinterface5g

Added graph in T_Tracer to observe UL-SNR calculated using SRS

common/utils/T/tracer/gnb.c
@@ -19,6 +19,7 @@ typedef struct {
   widget *pucch_pusch_iq_plot;
   widget *ul_freq_estimate_ue_xy_plot;
   widget *ul_time_estimate_ue_xy_plot;
+  widget *ul_snr_estimate_ue_xy_plot;
   widget *current_ue_label;
   widget *current_ue_button;
   widget *prev_ue_button;
@@ -26,6 +27,7 @@ typedef struct {
   logger *pucch_pusch_iq_logger;
   logger *ul_freq_estimate_ue_logger;
   logger *ul_time_estimate_ue_logger;
+  logger *ul_snr_ue_logger;
 } gnb_gui;
 
 typedef struct {
@@ -98,6 +100,9 @@ static void set_current_ue(gui *g, gnb_data *e, int ue)
 
   sprintf(s, "UL channel estimation in time domain [UE %d]", ue);
   xy_plot_set_title(g, e->e->ul_time_estimate_ue_xy_plot, s);
+
+  sprintf(s, "UL SNR per RB based on SRS [UE %d]", ue);
+  xy_plot_set_title(g, e->e->ul_snr_estimate_ue_xy_plot, s);
 }
 
 void reset_ue_ids(void)
@@ -139,6 +144,7 @@ static void gnb_main_gui(gnb_gui *e, gui *g, event_handler *h, void *database, g
   widget *main_window;
   widget *top_container;
   widget *line;
+  widget *line2;
   widget *col;
   widget *logo;
   widget *w;
@@ -146,12 +152,16 @@ static void gnb_main_gui(gnb_gui *e, gui *g, event_handler *h, void *database, g
   logger *l;
   view *v;
 
-  main_window = new_toplevel_window(g, 1500, 230, "gNB tracer");
+  main_window = new_toplevel_window(g, 1500, 460, "gNB tracer");
   top_container = new_container(g, VERTICAL);
   widget_add_child(g, main_window, top_container, -1);
 
   line = new_container(g, HORIZONTAL);
   widget_add_child(g, top_container, line, -1);
+
+  line2 = new_container(g, HORIZONTAL);
+  widget_add_child(g, top_container, line2, -1);
+
   logo = new_image(g, openair_logo_png, openair_logo_png_len);
 
   /* logo + prev/next UE buttons */
@@ -206,6 +216,17 @@ static void gnb_main_gui(gnb_gui *e, gui *g, event_handler *h, void *database, g
   logger_add_view(l, v);
   e->ul_time_estimate_ue_logger = l;
 
+  /* UL SNR based on SRS */
+  w = new_xy_plot(g, 1280, 200, "", 190);
+  e->ul_snr_estimate_ue_xy_plot = w;
+  widget_add_child(g, line2, w, -1);
+  xy_plot_set_range(g, w, 0, 273, -10, 65);
+  l = new_framelog(h, database, "GNB_PHY_UL_SNR_ESTIMATE", "subframe", "snr");
+  framelog_set_update_only_at_sf9(l, 0);
+  v = new_view_xy(273, 10, g, w, new_color(g, "#0c0c72"), XY_LOOP_MODE);
+  logger_add_view(l, v);
+  e->ul_snr_ue_logger = l;
+
   set_current_ue(g, ed, ed->ue);
   register_notifier(g, "click", e->current_ue_button, click, ed);
   register_notifier(g, "click", e->prev_ue_button, click, ed);
@@ -251,6 +272,7 @@ int main(int n, char **v)
   on_off(database, "GNB_PHY_PUCCH_PUSCH_IQ", is_on, 1);
   on_off(database, "GNB_PHY_UL_FREQ_CHANNEL_ESTIMATE", is_on, 1);
   on_off(database, "GNB_PHY_UL_TIME_CHANNEL_ESTIMATE", is_on, 1);
+  on_off(database, "GNB_PHY_UL_SNR_ESTIMATE", is_on, 1);
 
   gnb_data.ue = 0;
   gnb_data.e = &eg;

whitedevil-iith/oaiRAN_Project

common/utils/T/tracer/gnb.c
@@ -19,6 +19,7 @@ typedef struct {
   widget *pucch_pusch_iq_plot;
   widget *ul_freq_estimate_ue_xy_plot;
   widget *ul_time_estimate_ue_xy_plot;
+  widget *ul_snr_estimate_ue_xy_plot;
   widget *current_ue_label;
   widget *current_ue_button;
   widget *prev_ue_button;
@@ -26,6 +27,7 @@ typedef struct {
   logger *pucch_pusch_iq_logger;
   logger *ul_freq_estimate_ue_logger;
   logger *ul_time_estimate_ue_logger;
+  logger *ul_snr_ue_logger;
 } gnb_gui;
 
 typedef struct {
@@ -98,6 +100,9 @@ static void set_current_ue(gui *g, gnb_data *e, int ue)
 
   sprintf(s, "UL channel estimation in time domain [UE %d]", ue);
   xy_plot_set_title(g, e->e->ul_time_estimate_ue_xy_plot, s);
+
+  sprintf(s, "UL SNR per RB based on SRS [UE %d]", ue);
+  xy_plot_set_title(g, e->e->ul_snr_estimate_ue_xy_plot, s);
 }
 
 void reset_ue_ids(void)
@@ -146,12 +151,13 @@ static void gnb_main_gui(gnb_gui *e, gui *g, event_handler *h, void *database, g
   logger *l;
   view *v;
 
-  main_window = new_toplevel_window(g, 1500, 230, "gNB tracer");
+  main_window = new_toplevel_window(g, 1500, 460, "gNB tracer");
   top_container = new_container(g, VERTICAL);
   widget_add_child(g, main_window, top_container, -1);
 
   line = new_container(g, HORIZONTAL);
   widget_add_child(g, top_container, line, -1);
+
   logo = new_image(g, openair_logo_png, openair_logo_png_len);
 
   /* logo + prev/next UE buttons */
@@ -206,6 +212,21 @@ static void gnb_main_gui(gnb_gui *e, gui *g, event_handler *h, void *database, g
   logger_add_view(l, v);
   e->ul_time_estimate_ue_logger = l;
 
+  line = new_container(g, HORIZONTAL);
+  widget_add_child(g, top_container, line, -1);
+
+  /* UL SNR based on SRS */
+  w = new_xy_plot(g, 1280, 200, "", 190);
+  e->ul_snr_estimate_ue_xy_plot = w;
+  widget_add_child(g, line, w, -1);
+  xy_plot_set_range(g, w, 0, 273, -10, 65);
+  l = new_framelog(h, database, "GNB_PHY_UL_SNR_ESTIMATE", "subframe", "snr");
+  framelog_set_update_only_at_sf9(l, 0);
+  framelog_set_type_buffer_db(l);
+  v = new_view_xy(273, 10, g, w, new_color(g, "#0c0c72"), XY_LOOP_MODE);
+  logger_add_view(l, v);
+  e->ul_snr_ue_logger = l;
+
   set_current_ue(g, ed, ed->ue);
   register_notifier(g, "click", e->current_ue_button, click, ed);
   register_notifier(g, "click", e->prev_ue_button, click, ed);
@@ -251,6 +272,7 @@ int main(int n, char **v)
   on_off(database, "GNB_PHY_PUCCH_PUSCH_IQ", is_on, 1);
   on_off(database, "GNB_PHY_UL_FREQ_CHANNEL_ESTIMATE", is_on, 1);
   on_off(database, "GNB_PHY_UL_TIME_CHANNEL_ESTIMATE", is_on, 1);
+  on_off(database, "GNB_PHY_UL_SNR_ESTIMATE", is_on, 1);
 
   gnb_data.ue = 0;
   gnb_data.e = &eg;