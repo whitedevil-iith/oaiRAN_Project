64091c9a5125bc38c1d3bc1b83261cc1cd28ec50
francescomani <email@francescomani.it>
2025-11-28 16:21:06 +0100

    remove workaround to take into account first symbol index, necessary when only SS for SSB 0 was pre-computed

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_primitives.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_primitives.c
@@ -513,7 +513,7 @@ NR_sched_pdcch_t set_pdcch_structure(gNB_MAC_INST *gNB_mac,
 
   for (int i = 0; i < sps; i++) {
     if ((monitoringSymbolsWithinSlot >> (sps - 1 - i)) & 1) {
-      pdcch.StartSymbolIndex = ss->searchSpaceId == 0 ? i + type0_PDCCH_CSS_config->first_symbol_index : i;
+      pdcch.StartSymbolIndex = i;
       break;
     }
   }
